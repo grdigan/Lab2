ed542e584339de66f84b8e2b9e36d4a105b5f1d3
Gabby <gdigan2@illinois.edu>
2018-09-11 16:16:06 -0500

    I tried!!

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,13 +34,19 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
-        }
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                while (!maze.canMove()) {
+                    maze.turnRight(); // Implement your maze solving algorithm here
+                }
+                maze.move();
+            }
 
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
+            }
         }
     }
 }
